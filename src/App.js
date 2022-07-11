b3ab3f9adc7db1ab3bd99e8d238bc1420f737372
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-07-11 22:41:42 +0630

    import react-router-dom , components and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+//import routes and route
+import { Routes, Route } from "react-router-dom";
+
+//import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+//import pages
+import Home from "./pages/Home";
+import PropertyDetails from "./pages/PropertyDetails";
+
 const App = () => {
 	return (
-		<div>
-			<div>App</div>
+		<div className="max-w-[1240px] mx-auto bg-white">
+			<Header />
+			<Routes>
+				<Route path="/" element={Home} />
+				<Route path="/property/:id" element={PropertyDetails} />
+			</Routes>
+			<Footer />
 		</div>
 	);
 };
